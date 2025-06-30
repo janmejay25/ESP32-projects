4f07d9588034a649290a2bef2db12cc6ba12eeb6
janmejay25 <janmjaypandya@gmail.com>
2025-06-30 21:10:12 +0530

    toggle

--- Blink.ino
+++ Blink.ino
@@ -5,6 +5,7 @@ void setup() {
   pinMode(2, OUTPUT);
 }
 
+
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
